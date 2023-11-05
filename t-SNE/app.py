4b464e7269e7b6113939a246bce505077ee0b405
fbizza <francesco.bizzarri@stud.unifi.it>
2023-11-05 23:09:29 +0100

    Implement interactive image description

--- t-SNE/app.py
+++ t-SNE/app.py
@@ -8,13 +8,14 @@ from dash import Dash, dcc, html, Input, Output
 from import_data import tsne_data
 
 # Import data and create Pandas dataframe
-x, y, coarse_labels, coarse_categories, images = tsne_data(N_IMAGES_PER_CLASS=15)
+x, y, coarse_labels, coarse_categories, fine_categories, images = tsne_data(N_IMAGES_PER_CLASS=30)
 
 data = {
     't_sne_x': x,
     't_sne_y': y,
     'coarse_label': coarse_labels,
     'coarse_category': coarse_categories,
+    'fine_category': fine_categories,
     'image': images.tolist()
 }
 
@@ -24,13 +25,14 @@ df = pd.DataFrame(data)
 app = Dash(__name__, external_stylesheets=[dbc.themes.LITERA])
 
 app.layout = dbc.Container([
-    html.H3('Interactive t-SNE plot of CIFAR-10 dataset'),
+    html.H3('Interactive t-SNE plot of CIFAR-100 dataset'),
+    html.H6('Click on single points to display their descriptions', style={'opacity': '0.80'}),
     dbc.Row([
         dbc.Col(dcc.Graph(id="scatter-plot"), width=8),
         dbc.Col([
-            # TODO:USE FINE LABELS TO CREATE AN IMAGE DESCRIPTION
             html.H6("Image Description", style={'text-align': 'center'}),
-            html.Div(html.Img(id='clicked-image', style={'height': '40%', 'width': '40%', 'display': 'block', 'margin': '0 auto'}))
+            html.Div(html.Img(id='clicked-image', style={'height': '40%', 'width': '40%', 'display': 'block', 'margin': '0 auto'})),
+            html.Div(id='image-text-description', style={'text-align': 'center'})
         ], width=4),
     ]),
     dbc.Row([
@@ -41,10 +43,13 @@ app.layout = dbc.Container([
             marks={0: '0', len(df.index): f'{len(df.index)}'},
             value=10,  # Initial value for the number of points to show
             tooltip={"placement": "bottom", "always_visible": True}
-        )
+        ),
+        html.H6("Created by Stijn Oosterlinck, Justine Rayp and Francesco Bizzarri")
     ]),
 ])
 
+
+# Callback for showing image of clicked point
 @app.callback(
     Output("clicked-image", 'src'),
     Input("scatter-plot", "clickData"))
@@ -56,7 +61,16 @@ def show_clicked_image(clickData):
         encoded_image = base64.b64encode(open("clicked-image.png", 'rb').read()).decode('ascii')
         return 'data:image/png;base64,{}'.format(encoded_image)
 
+# Callback for updating image description
+@app.callback(
+    Output("image-text-description", 'children'),
+    Input("scatter-plot", "clickData"))
+def show_clicked_image(clickData):
+    if clickData:
+        fine_category = clickData['points'][0]['customdata'][1]
+        return f'This picture shows a {fine_category}'
 
+# Callback for updating scatter plot based on slider number selection
 @app.callback(
     Output("scatter-plot", "figure"),
     Input("max-slider", "value"))
@@ -69,7 +83,7 @@ def update_scatter_plot(num_points_high):
     fig = px.scatter(selected_points, x='t_sne_x', y='t_sne_y',
                      color='coarse_category',
                      hover_data=['coarse_category'],
-                     custom_data=['image'],
+                     custom_data=['image', 'fine_category'],
                      labels={
                          'coarse_category': 'Coarse Category',
                          't_sne_x': 't-SNE first dimension',
--- t-SNE/import_data.py
+++ t-SNE/import_data.py
@@ -26,6 +26,108 @@ coarse_to_category = {
     18: 'vehicles 1',
     19: 'vehicles 2'
 }
+fine_to_cateogry = {
+    0: 'apple',
+    1: 'aquarium_fish',
+    2: 'baby',
+    3: 'bear',
+    4: 'beaver',
+    5: 'bed',
+    6: 'bee',
+    7: 'beetle',
+    8: 'bicycle',
+    9: 'bottle',
+    10: 'bowl',
+    11: 'boy',
+    12: 'bridge',
+    13: 'bus',
+    14: 'butterfly',
+    15: 'camel',
+    16: 'can',
+    17: 'castle',
+    18: 'caterpillar',
+    19: 'cattle',
+    20: 'chair',
+    21: 'chimpanzee',
+    22: 'clock',
+    23: 'cloud',
+    24: 'cockroach',
+    25: 'couch',
+    26: 'crab',
+    27: 'crocodile',
+    28: 'cup',
+    29: 'dinosaur',
+    30: 'dolphin',
+    31: 'elephant',
+    32: 'flatfish',
+    33: 'forest',
+    34: 'fox',
+    35: 'girl',
+    36: 'hamster',
+    37: 'house',
+    38: 'kangaroo',
+    39: 'computer_keyboard',
+    40: 'lamp',
+    41: 'lawn_mower',
+    42: 'leopard',
+    43: 'lion',
+    44: 'lizard',
+    45: 'lobster',
+    46: 'man',
+    47: 'maple_tree',
+    48: 'motorcycle',
+    49: 'mountain',
+    50: 'mouse',
+    51: 'mushroom',
+    52: 'oak_tree',
+    53: 'orange',
+    54: 'orchid',
+    55: 'otter',
+    56: 'palm_tree',
+    57: 'pear',
+    58: 'pickup_truck',
+    59: 'pine_tree',
+    60: 'plain',
+    61: 'plate',
+    62: 'poppy',
+    63: 'porcupine',
+    64: 'possum',
+    65: 'rabbit',
+    66: 'raccoon',
+    67: 'ray',
+    68: 'road',
+    69: 'rocket',
+    70: 'rose',
+    71: 'sea',
+    72: 'seal',
+    73: 'shark',
+    74: 'shrew',
+    75: 'skunk',
+    76: 'skyscraper',
+    77: 'snail',
+    78: 'snake',
+    79: 'spider',
+    80: 'squirrel',
+    81: 'streetcar',
+    82: 'sunflower',
+    83: 'sweet_pepper',
+    84: 'table',
+    85: 'tank',
+    86: 'telephone',
+    87: 'television',
+    88: 'tiger',
+    89: 'tractor',
+    90: 'train',
+    91: 'trout',
+    92: 'tulip',
+    93: 'turtle',
+    94: 'wardrobe',
+    95: 'whale',
+    96: 'willow_tree',
+    97: 'wolf',
+    98: 'woman',
+    99: 'worm'
+}
 
 np.random.seed(seed=666)
 
@@ -42,6 +144,7 @@ def tsne_data(N_IMAGES_PER_CLASS=10):
 
     x_train = x_train_coarse[idx]
     y_train = coarse_labels_train[idx]
+    y_train_fine = fine_labels_train[idx]
 
     ##### ----- Uncomment this part to plot an example image for each class ----- #####
 
@@ -60,9 +163,10 @@ def tsne_data(N_IMAGES_PER_CLASS=10):
     x = tsne[:, 0]
     y = tsne[:, 1]
     coarse_labels = y_train  # These are numbers from 0 to 19
+    fine_labels = y_train_fine  # These are numbers from 0 to 99
     coarse_categories = [coarse_to_category[label] for label in coarse_labels.flatten()]  # These are strings
-
-    return x, y, coarse_labels.flatten().tolist(), coarse_categories, x_train.reshape((len(x_train), 32 * 32 * 3))
+    fine_categories = [fine_to_cateogry[label] for label in fine_labels.flatten()]  # These are strings
+    return x, y, coarse_labels.flatten().tolist(), coarse_categories, fine_categories, x_train.reshape((len(x_train), 32 * 32 * 3))
 
 
 
