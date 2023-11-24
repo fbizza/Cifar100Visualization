c4eb4a3708fd621c98468ae60bfe1e0a4612ac9d
fbizza <bizzafrancesco@gmail.com>
2023-11-24 22:01:50 +0100

    Compute t-sne of feature vector instead of raw pixels

---
+++ XAI/app.py
@@ -0,0 +1,106 @@
+import dash_bootstrap_components as dbc
+import plotly.express as px
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import base64
+import dash
+from dash import Dash, dcc, html, Input, Output, State
+from import_data import tsne_data
+
+# Import data and create Pandas dataframe
+x, y, coarse_labels, coarse_categories, fine_categories, images = tsne_data(N_IMAGES_PER_CLASS=10)
+
+data = {
+    't_sne_x': x,
+    't_sne_y': y,
+    'coarse_label': coarse_labels,
+    'coarse_category': coarse_categories,
+    'fine_category': fine_categories,
+    'image': images.tolist()
+}
+
+df = pd.DataFrame(data)
+
+# Create app
+app = Dash(__name__, external_stylesheets=[dbc.themes.LITERA])
+
+app.layout = dbc.Container([
+    html.H3('Interactive t-SNE plot of CIFAR-100 dataset'),
+    html.H6('Click on single points to display their descriptions', style={'opacity': '0.80'}),
+    dbc.Row([
+        dbc.Col(dcc.Graph(id="scatter-plot"), width=8),
+        dbc.Col([
+            html.H6("Image Description", style={'text-align': 'center'}),
+            html.Div(html.Img(id='clicked-image', style={'height': '40%', 'width': '40%', 'display': 'block', 'margin': '0 auto'})),
+            html.Div(id='image-text-description', style={'text-align': 'center'})
+        ], width=4),
+    ]),
+    dbc.Row([
+        html.H5("Number of points to show:", style={'text-align': 'center'}),
+        dcc.Slider(
+            id='max-slider',
+            min=0, max=len(df.index), step=1,
+            marks={0: '0', len(df.index): f'{len(df.index)}'},
+            value=100,  # Initial value for the number of points to show
+            tooltip={"placement": "bottom", "always_visible": True}
+        ),
+        dbc.Button("Update Plot", id="update-plot-button", style={'width': '10%', 'margin': '0 auto'}),
+        html.H6("Created by Stijn Oosterlinck, Justine Rayp and Francesco Bizzarri",
+                style={'margin-top': '2.5em', 'font-size': '0.8em', 'font-weight': 'lighter'})
+    ]),
+])
+
+
+# Callback for showing image of clicked point
+@app.callback(
+    Output("clicked-image", 'src'),
+    Input("scatter-plot", "clickData"))
+def show_clicked_image(clickData):
+    if clickData:
+        image_data = np.array(clickData['points'][0]['customdata'][0], dtype='uint8')
+        image = image_data.reshape(32, 32, 3)
+        plt.imsave("clicked-image.png", image)
+        encoded_image = base64.b64encode(open("clicked-image.png", 'rb').read()).decode('ascii')
+        return 'data:image/png;base64,{}'.format(encoded_image)
+
+# Callback for updating image description
+@app.callback(
+    Output("image-text-description", 'children'),
+    Input("scatter-plot", "clickData"))
+def show_clicked_image(clickData):
+    if clickData:
+        fine_category = clickData['points'][0]['customdata'][1]
+        return f'This picture shows a {fine_category}'
+
+# Callback for updating scatter plot based on slider number selection and button click
+@app.callback(
+    Output("scatter-plot", "figure"),
+    Input("update-plot-button", "n_clicks"),
+    State("max-slider", "value")
+)
+def update_scatter_plot_on_button_click(n_clicks, value):
+
+    num_points_to_show = value  # Value from slider
+
+    selected_points = df.sample(n=num_points_to_show)
+
+    fig = px.scatter(selected_points, x='t_sne_x', y='t_sne_y',
+                     color='coarse_category',
+                     hover_data=['coarse_category'],
+                     custom_data=['image', 'fine_category'],
+                     labels={
+                         'coarse_category': 'Coarse Category',
+                         't_sne_x': 't-SNE first dimension',
+                         't_sne_y': 't-SNE second dimension'},
+                     color_discrete_sequence=px.colors.qualitative.Light24)
+
+    fig.update_xaxes(showgrid=False)
+    fig.update_yaxes(showgrid=False)
+
+    return fig
+
+
+app.run_server(debug=True)
+
+
---
+++ XAI/import_data.py
@@ -0,0 +1,174 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
+from model import compute_features_vectors
+
+coarse_to_category = {
+    0: 'aquatic mammals',
+    1: 'fish',
+    2: 'flowers',
+    3: 'food containers',
+    4: 'fruit and vegetables',
+    5: 'household electrical devices',
+    6: 'household furniture',
+    7: 'insects',
+    8: 'large carnivores',
+    9: 'large man-made outdoor things',
+    10: 'large natural outdoor scenes',
+    11: 'large omnivores and herbivores',
+    12: 'medium-sized mammals',
+    13: 'non-insect invertebrates',
+    14: 'people',
+    15: 'reptiles',
+    16: 'small mammals',
+    17: 'trees',
+    18: 'vehicles 1',
+    19: 'vehicles 2'
+}
+fine_to_cateogry = {
+    0: 'apple',
+    1: 'aquarium_fish',
+    2: 'baby',
+    3: 'bear',
+    4: 'beaver',
+    5: 'bed',
+    6: 'bee',
+    7: 'beetle',
+    8: 'bicycle',
+    9: 'bottle',
+    10: 'bowl',
+    11: 'boy',
+    12: 'bridge',
+    13: 'bus',
+    14: 'butterfly',
+    15: 'camel',
+    16: 'can',
+    17: 'castle',
+    18: 'caterpillar',
+    19: 'cattle',
+    20: 'chair',
+    21: 'chimpanzee',
+    22: 'clock',
+    23: 'cloud',
+    24: 'cockroach',
+    25: 'couch',
+    26: 'crab',
+    27: 'crocodile',
+    28: 'cup',
+    29: 'dinosaur',
+    30: 'dolphin',
+    31: 'elephant',
+    32: 'flatfish',
+    33: 'forest',
+    34: 'fox',
+    35: 'girl',
+    36: 'hamster',
+    37: 'house',
+    38: 'kangaroo',
+    39: 'computer_keyboard',
+    40: 'lamp',
+    41: 'lawn_mower',
+    42: 'leopard',
+    43: 'lion',
+    44: 'lizard',
+    45: 'lobster',
+    46: 'man',
+    47: 'maple_tree',
+    48: 'motorcycle',
+    49: 'mountain',
+    50: 'mouse',
+    51: 'mushroom',
+    52: 'oak_tree',
+    53: 'orange',
+    54: 'orchid',
+    55: 'otter',
+    56: 'palm_tree',
+    57: 'pear',
+    58: 'pickup_truck',
+    59: 'pine_tree',
+    60: 'plain',
+    61: 'plate',
+    62: 'poppy',
+    63: 'porcupine',
+    64: 'possum',
+    65: 'rabbit',
+    66: 'raccoon',
+    67: 'ray',
+    68: 'road',
+    69: 'rocket',
+    70: 'rose',
+    71: 'sea',
+    72: 'seal',
+    73: 'shark',
+    74: 'shrew',
+    75: 'skunk',
+    76: 'skyscraper',
+    77: 'snail',
+    78: 'snake',
+    79: 'spider',
+    80: 'squirrel',
+    81: 'streetcar',
+    82: 'sunflower',
+    83: 'sweet_pepper',
+    84: 'table',
+    85: 'tank',
+    86: 'telephone',
+    87: 'television',
+    88: 'tiger',
+    89: 'tractor',
+    90: 'train',
+    91: 'trout',
+    92: 'tulip',
+    93: 'turtle',
+    94: 'wardrobe',
+    95: 'whale',
+    96: 'willow_tree',
+    97: 'wolf',
+    98: 'woman',
+    99: 'worm'
+}
+
+np.random.seed(seed=666)
+
+(x_train_fine, fine_labels_train), (x_test_fine, fine_labels_test) = tf.keras.datasets.cifar100.load_data(label_mode='fine')
+(x_train_coarse, coarse_labels_train), (x_test_coarse, coarse_labels_test) = tf.keras.datasets.cifar100.load_data(label_mode='coarse')
+
+def tsne_data(N_IMAGES_PER_CLASS):
+
+    # Loop over each class label and sample N_IMAGES_PER_CLASS random images over each class
+    idx = np.empty(0, dtype="int8")
+    for i in range(0, len(np.unique(coarse_labels_train))):
+        idx = np.append(idx, np.random.choice(np.where((coarse_labels_train[0:len(coarse_labels_train)]) == i)[0],
+                                              N_IMAGES_PER_CLASS, replace=False))
+
+    x_train = x_train_coarse[idx]
+    y_train = coarse_labels_train[idx]
+    y_train_fine = fine_labels_train[idx]
+
+    # #### ----- Uncomment this part to plot an example image for each class ----- #####
+    #
+    # plt.figure(figsize=(20, 20))
+    # for i in range(len(np.unique(y_train))):
+    #     class_indices = np.where(y_train == i)[0]
+    #     random_index = np.random.choice(class_indices, 1)
+    #     plt.subplot(1, 20, i + 1)
+    #     img = x_train[random_index].reshape(32, 32, 3)
+    #     plt.imshow(img)
+    #     plt.axis('off')
+    # plt.show()
+    print(f"x_train shape:{x_train.shape}")
+    model = TSNE(n_components=2)
+    images = compute_features_vectors(x_train, 'activation_14')
+    tsne = model.fit_transform(images.reshape((len(x_train), -1)))
+    x = tsne[:, 0]
+    y = tsne[:, 1]
+    coarse_labels = y_train  # These are numbers from 0 to 19
+    fine_labels = y_train_fine  # These are numbers from 0 to 99
+    coarse_categories = [coarse_to_category[label] for label in coarse_labels.flatten()]  # These are strings
+    fine_categories = [fine_to_cateogry[label] for label in fine_labels.flatten()]  # These are strings
+    return x, y, coarse_labels.flatten().tolist(), coarse_categories, fine_categories, x_train.reshape((len(x_train), 32 * 32 * 3))
+
+
+
+
